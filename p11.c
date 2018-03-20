975e0c040a26f88541308f7b015f5f745d450f7b
Ramyaveltamil <31531425+Ramyaveltamil@users.noreply.github.com>
2018-03-20 15:56:09 +0530

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char str[20];
+    printf("Enter the day:");
+    scanf("%s",str);
+    if((str[0]=='s'))
+    printf("Yes");
+    else
+    printf("No"); 
+    return 0;
+}
